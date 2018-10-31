dd050465aec84c46f1d958f3c7ab07b573553d8e
dreeeeam <41610065+dreeeeam@users.noreply.github.com>
2018-10-31 16:48:53 +0800

    Add files via upload

---
+++ ArrayQueue/QueueInterface.java
@@ -0,0 +1,12 @@
+
+package ArrayQueue;
+
+
+public interface QueueInterface {
+    
+    public void add(Object object);
+    public Object first();
+    public Object remove();
+    public int size();
+    public boolean isEmpty();
+}
